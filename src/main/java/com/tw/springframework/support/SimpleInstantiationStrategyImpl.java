a98b1ff99bfe155be0a4c22eb5d74cdc8b685aa3
taowei <mister.taowei@gmail.com>
2021-11-29 20:35:11 +0800

    编写SimpleInstantiationStrategyImpl使用jdk自带的反射调用有参构造函数创建对象

---
+++ src/main/java/com/tw/springframework/support/SimpleInstantiationStrategyImpl.java
@@ -0,0 +1,23 @@
+package com.tw.springframework.support;
+
+import com.tw.springframework.config.InstantiationStrategy;
+import com.tw.springframework.exception.BeansException;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class SimpleInstantiationStrategyImpl implements InstantiationStrategy {
+    @Override
+    public Object instantiate(BeanDefinition beanDefinition, Constructor constructor, Object... args) {
+        Class beanClass = beanDefinition.getBeanClass();
+        try {
+            if (constructor != null) {
+                return constructor.newInstance(args);
+            } else {
+                return beanClass.getDeclaredConstructor().newInstance();
+            }
+        } catch (InstantiationException | IllegalAccessException | InvocationTargetException | NoSuchMethodException e) {
+            throw new BeansException();
+        }
+    }
+}
